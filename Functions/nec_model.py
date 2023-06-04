e6741a6bd76e0f179dcc62ca61e0b8644f804ac2
Hallvard A. Wæhler <60000604+hallvaaw@users.noreply.github.com>
2023-06-04 16:59:31 +0200

    Update parameters

--- Functions/nec_model.py
+++ Functions/nec_model.py
@@ -1,6 +1,6 @@
 import math
 
-def nec_model(x, c, b, d, e):
+def nec_model(x, c, b, d, e, b_2, d_2, e_2):
     """Calculates no effect concentration model"""
-    f = c + (d - c) * math.exp(-b(x - e) * I * (x - e)) + ((d * 2) / (1 + math.exp((b*2) * (math.log(x))) - math.log(e*2)))
+    f = c + (d - c) * math.exp(-b(x - e) * I * (x - e)) + ((d_2) / (1 + math.exp((b_2) * (math.log(x))) - math.log(e_2)))
     return f
