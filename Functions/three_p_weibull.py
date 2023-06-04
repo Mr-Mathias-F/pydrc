c80b64609dee25a8210e7990359f2ea456b23035
Hallvard A. Wæhler <60000604+hallvaaw@users.noreply.github.com>
2023-06-04 17:14:13 +0200

    Update parameters

--- Functions/three_p_weibull.py
+++ Functions/three_p_weibull.py
@@ -1,6 +1,6 @@
 import math
 
-def three_p_weibull(x, b, e):
+def three_p_weibull(x, b, d, e):
     """Calculate value of three-parameter Weibull function"""
     f = 0 + (d - 0) * math.exp(-math.exp(b * (log(x) - e)))
     return f
