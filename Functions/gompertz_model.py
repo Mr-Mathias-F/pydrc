2997498603c615287cc861b0b473ef069fa23a7c
Hallvard A. Wæhler <60000604+hallvaaw@users.noreply.github.com>
2023-07-19 23:20:49 +0200

    Fix syntax error

--- Functions/gompertz_model.py
+++ Functions/gompertz_model.py
@@ -1,6 +1,6 @@
 import math
 
-def gompertz_model(x, c, d, e):
+def gompertz_model(x, b, c, d, e):
     """Calculate value of Gompertz model"""
     f = c + (d - c) * (math.exp(-math.exp(b * (x - e))))
     return f
