e762f62d0bb4ea5a33d9019746157115bb7d86d7
Hallvard A. Wæhler <60000604+hallvaaw@users.noreply.github.com>
2023-07-19 23:24:11 +0200

    Fix syntax error

--- Functions/nec_model.py
+++ Functions/nec_model.py
@@ -1,6 +1,6 @@
 import math
 
-def nec_model(x, c, b, d, e, b_2, d_2, e_2):
+def nec_model(x, c, b, d, e, b_2, d_2, e_2, I):
     """Calculates no effect concentration model"""
     f = c + (d - c) * math.exp(-b(x - e) * I * (x - e)) + ((d_2) / (1 + math.exp((b_2) * (math.log(x))) - math.log(e_2)))
     return f
