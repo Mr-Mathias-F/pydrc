7d024226232d55247d232152dacb6252e69835bd
Hallvard A. Wæhler <60000604+hallvaaw@users.noreply.github.com>
2023-07-19 23:25:05 +0200

    Fix syntax error

--- Functions/weibull_P3.py
+++ Functions/weibull_P3.py
@@ -2,5 +2,5 @@ import math
 
 def weibull_P3(x, b, d, e):
     """Calculate value of three-parameter Weibull function"""
-    f = 0 + (d - 0) * math.exp(-math.exp(b * (log(x) - e)))
+    f = 0 + (d - 0) * math.exp(-math.exp(b * (math.log(x) - e)))
     return f
