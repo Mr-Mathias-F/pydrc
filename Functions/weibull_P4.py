ee74e041a52cae6250495ec0187125f954cc7be6
Hallvard A. Wæhler <60000604+hallvaaw@users.noreply.github.com>
2023-07-19 23:15:11 +0200

    Fix syntax error

--- Functions/weibull_P4.py
+++ Functions/weibull_P4.py
@@ -1,6 +1,6 @@
 import math
 
-def weibull_P4(, b, c, d, e):
+def weibull_P4(b, c, d, e):
     """Calculate value of four-parameter Weibull function"""
     f = c + (d - c) * math.exp(-math.exp(b * (math.log(x) - math.log(e))))
     return f
