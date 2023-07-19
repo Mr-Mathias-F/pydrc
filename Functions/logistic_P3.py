1d6fd086b5d98e9064c716f68e61f364878dcab7
Hallvard A. Wæhler <60000604+hallvaaw@users.noreply.github.com>
2023-07-19 23:22:54 +0200

    Fix syntax error

--- Functions/logistic_P3.py
+++ Functions/logistic_P3.py
@@ -1,6 +1,6 @@
 import math
 
-def logistic_P3(x, b, e):
+def logistic_P3(x, b, c, e):
     """Calculates the log-logistic curve (2-parameter model)"""
     f = c + (1 - c)/(1 + math.exp(b*(math.log(x) - e)))
     return f
