d90a97d687e766b5b23bfc90edf3f8c89524be00
Hallvard A. Wæhler <60000604+hallvaaw@users.noreply.github.com>
2023-09-17 22:49:03 +0200

    Update logistic_P4 to class

--- Functions/logistic_P4.py
+++ Functions/logistic_P4.py
@@ -1,6 +1,25 @@
 import math
+import numpy as np
+from scipy.optimize import curve_fit
 
-def logistic_P4(x, b, c, d, e):
-    """Calculates the log-logistic curve (4-parameter model)"""
-    f = c + (d - c)/(1 + math.exp(b*(math.log(x) - math.log(e))))
-    return f
+class LogisticP4Model:
+    def __init__(self):
+        """Initialize a LogisticP4Model instance with parameters set to None"""
+        self.params = None
+
+    def logistic_P4(self, x, b, c, d, e):
+        """Calculate the log-logistic curve (4-parameter model) at a given point"""
+        f = c + (d - c) / (1 + np.exp(b * (np.log(x) - np.log(e))))
+        return f
+
+    def fit(self, x_data, y_data):
+        """Estimate the parameters (b, c, d, e) of the log-logistic curve from data"""
+        params, _ = curve_fit(self.logistic_P4, x_data, y_data)
+        self.params = params
+
+    def predict(self, x):
+        """Predict the output of the log-logistic curve for a given input using estimated parameters"""
+        if self.params is None:
+            raise ValueError("Parameters not estimated. Call fit() first.")
+        b, c, d, e = self.params
+        return self.logistic_P4(x, b, c, d, e)
