22e23c10f0be056369e6c84875581d060ef971de
Hallvard A. Wæhler <60000604+hallvaaw@users.noreply.github.com>
2023-10-02 22:15:14 +0200

    Update nec_model.py to class

--- Functions/nec_model.py
+++ Functions/nec_model.py
@@ -1,6 +1,25 @@
 import math
+import numpy as np
+from scipy.optimize import curve_fit
 
-def nec_model(x, c, b, d, e, b_2, d_2, e_2, I):
-    """Calculates no effect concentration model"""
-    f = c + (d - c) * math.exp(-b(x - e) * I * (x - e)) + ((d_2) / (1 + math.exp((b_2) * (math.log(x))) - math.log(e_2)))
-    return f
+class NecModel:
+    def __init__(self):
+        """Initialize a NecModel instance with parameters set to None"""
+        self.params = None
+
+    def nec_model(self, x, c, b, d, e, b_2, d_2, e_2, I):
+        """Calculate the no effect concentration model at a given point"""
+        f = c + (d - c) * np.exp(-b * (x - e) * I * (x - e)) + ((d_2) / (1 + np.exp(b_2 * (np.log(x)) - np.log(e_2))))
+        return f
+
+    def fit(self, x_data, y_data):
+        """Estimate the parameters (c, b, d, e, b_2, d_2, e_2, I) of the no effect concentration model from data"""
+        params, _ = curve_fit(self.nec_model, x_data, y_data)
+        self.params = params
+
+    def predict(self, x):
+        """Predict the output of the no effect concentration model for a given input using estimated parameters"""
+        if self.params is None:
+            raise ValueError("Parameters not estimated. Call fit() first.")
+        c, b, d, e, b_2, d_2, e_2, I = self.params
+        return self.nec_model(x, c, b, d, e, b_2, d_2, e_2, I)
