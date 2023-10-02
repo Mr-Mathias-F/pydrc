0da106adff8d9a5bbca1835fa0ef9935404b88be
Hallvard A. Wæhler <60000604+hallvaaw@users.noreply.github.com>
2023-10-02 22:11:25 +0200

    Update michaelis_menten.py to class

--- Functions/michaelis_menten.py
+++ Functions/michaelis_menten.py
@@ -1,6 +1,23 @@
-def michaelis_menten(v_max, k_m, s):
-    """
-    Calculates the rate of an enzymatic reaction using the Michaelis-Menten equation
-    """
-    v_0 = (v_max * s) / (k_m + s)
-    return v_0
+from scipy.optimize import curve_fit
+
+class MichaelisMenten:
+    def __init__(self):
+        """Initialize a MichaelisMentenModel instance with parameters set to None"""
+        self.params = None
+
+    def michaelis_menten(self, x, v_max, k_m):
+        """Calculate the Michaelis-Menten rate at a given point"""
+        v_0 = (v_max * x) / (k_m + x)
+        return v_0
+
+    def fit(self, x_data, y_data):
+        """Estimate the parameters (v_max, k_m) of the Michaelis-Menten equation from data"""
+        params, _ = curve_fit(self.michaelis_menten, x_data, y_data)
+        self.params = params
+
+    def predict(self, x):
+        """Predict the rate of the Michaelis-Menten equation for a given input using estimated parameters"""
+        if self.params is None:
+            raise ValueError("Parameters not estimated. Call fit() first.")
+        v_max, k_m = self.params
+        return self.michaelis_menten(x, v_max, k_m)
