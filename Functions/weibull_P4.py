bd895c59b722ebc7b1997f7322f3665b03d47de0
Hallvard A. Wæhler <60000604+hallvaaw@users.noreply.github.com>
2023-10-02 22:14:06 +0200

    Update weibull_P4.py as class

--- Functions/weibull_P4.py
+++ Functions/weibull_P4.py
@@ -1,6 +1,24 @@
-import math
+from scipy.optimize import curve_fit
+import numpy as np
 
-def weibull_P4(x, b, c, d, e):
-    """Calculate value of four-parameter Weibull function"""
-    f = c + (d - c) * math.exp(-math.exp(b * (math.log(x) - math.log(e))))
-    return f
+class WeibullP4:
+    def __init__(self):
+        """Initialize a WeibullP4Model instance with parameters set to None"""
+        self.params = None
+
+    def weibull_P4(self, x, b, c, d, e):
+        """Calculate the four-parameter Weibull function at a given point"""
+        f = c + (d - c) * np.exp(-np.exp(b * (np.log(x) - np.log(e))))
+        return f
+
+    def fit(self, x_data, y_data):
+        """Estimate the parameters (b, c, d, e) of the four-parameter Weibull function from data"""
+        params, _ = curve_fit(self.weibull_P4, x_data, y_data)
+        self.params = params
+
+    def predict(self, x):
+        """Predict the output of the four-parameter Weibull function for a given input using estimated parameters"""
+        if self.params is None:
+            raise ValueError("Parameters not estimated. Call fit() first.")
+        b, c, d, e = self.params
+        return self.weibull_P4(x, b, c, d, e)
