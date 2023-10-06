ded08f9df1b0359dfcd825103ac33a57ff36491e
Hallvard A. Wæhler <60000604+hallvaaw@users.noreply.github.com>
2023-10-06 12:28:08 +0200

    Update gompertz_model.py

--- Functions/gompertz_model.py
+++ Functions/gompertz_model.py
@@ -4,21 +4,20 @@ from scipy.optimize import curve_fit
 
 class GompertzModel:
     def __init__(self):
-        """Initialize a GompertzModel instance with parameters set to None"""
         self.params = None
 
     def gompertz_model(self, x, b, c, d, e):
-        """Calculate the Gompertz model at a given point"""
+        """Calculate Gompertz model"""
         f = c + (d - c) * np.exp(-np.exp(b * (x - e)))
         return f
 
     def fit(self, x_data, y_data):
-        """Estimate the parameters (b, c, d, e) of the Gompertz model from data"""
+        """Estimate parameters (b, c, d, e)"""
         params, _ = curve_fit(self.gompertz_model, x_data, y_data)
         self.params = params
 
     def predict(self, x):
-        """Predict the output of the Gompertz model for a given input using estimated parameters"""
+        """Predict output of the Gompertz model"""
         if self.params is None:
             raise ValueError("Parameters not estimated. Call fit() first.")
         b, c, d, e = self.params
