8bb25306dcc414881b0b67d296da4f5eee0aeba7
Hallvard A. Wæhler <60000604+hallvaaw@users.noreply.github.com>
2023-10-06 12:25:56 +0200

    Update ar_model.py

--- Functions/ar_model.py
+++ Functions/ar_model.py
@@ -4,21 +4,20 @@ from scipy.optimize import curve_fit
 
 class ARModel:
     def __init__(self):
-        """Initialize an ARModel instance with parameters set to None"""
         self.params = None
 
     def ar_model(self, x, c, d, e):
-        """Calculate the asymptotic regression model at a given point"""
+        """Calculate asymptotic regression model"""
         f = c + (d - c) * (1 - np.exp(-x / e))
         return f
 
     def fit(self, x_data, y_data):
-        """Estimate the parameters (c, d, e) of the asymptotic regression model from data"""
+        """Estimate the parameters (c, d, e)"""
         params, _ = curve_fit(self.ar_model, x_data, y_data)
         self.params = params
 
     def predict(self, x):
-        """Predict the output of the asymptotic regression model for a given input using estimated parameters"""
+        """Predict output of the asymptotic regression model"""
         if self.params is None:
             raise ValueError("Parameters not estimated. Call fit() first.")
         c, d, e = self.params
