b522e6656e36317f82af7efeeabeab837f5526d2
Hallvard A. Wæhler <60000604+hallvaaw@users.noreply.github.com>
2023-10-06 12:30:47 +0200

    Update logistic_P2.py

--- Functions/logistic_P2.py
+++ Functions/logistic_P2.py
@@ -4,21 +4,20 @@ from scipy.optimize import curve_fit
 
 class LogisticP2:
     def __init__(self):
-        """Initialize a LogisticP2Model instance with parameters set to None"""
         self.params = None
 
     def logistic_P2(self, x, b, e):
-        """Calculate the log-logistic curve (2-parameter model) at a given point"""
+        """Calculate log-logistic curve"""
         f = 1 / (1 + np.exp(b * (np.log(x) - np.log(e))))
         return f
 
     def fit(self, x_data, y_data):
-        """Estimate the parameters (b, e) of the log-logistic curve (2-parameter model) from data"""
+        """Estimate parameters (b, e)"""
         params, _ = curve_fit(self.logistic_P2, x_data, y_data)
         self.params = params
 
     def predict(self, x):
-        """Predict the output of the log-logistic curve (2-parameter model) for a given input using estimated parameters"""
+        """Predict output of log-logistic curve (2-parameter model)"""
         if self.params is None:
             raise ValueError("Parameters not estimated. Call fit() first.")
         b, e = self.params
