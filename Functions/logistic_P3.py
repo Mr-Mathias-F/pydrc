b8faaa9e43dead936ff1f16ab0185c43c250f2ef
Hallvard A. Wæhler <60000604+hallvaaw@users.noreply.github.com>
2023-10-06 12:31:32 +0200

    Update logistic_P3.py

--- Functions/logistic_P3.py
+++ Functions/logistic_P3.py
@@ -4,21 +4,20 @@ from scipy.optimize import curve_fit
 
 class LogisticP3:
     def __init__(self):
-        """Initialize a LogisticP3Model instance with parameters set to None"""
         self.params = None
 
     def logistic_P3(self, x, b, c, e):
-        """Calculate the log-logistic curve (3-parameter model) at a given point"""
+        """Calculate log-logistic curve"""
         f = c + (1 - c) / (1 + np.exp(b * (np.log(x) - e)))
         return f
 
     def fit(self, x_data, y_data):
-        """Estimate the parameters (b, c, e) of the log-logistic curve (3-parameter model) from data"""
+        """Estimate parameters (b, c, e)"""
         params, _ = curve_fit(self.logistic_P3, x_data, y_data)
         self.params = params
 
     def predict(self, x):
-        """Predict the output of the log-logistic curve (3-parameter model) for a given input using estimated parameters"""
+        """Predict output of the log-logistic curve (3-parameter model)"""
         if self.params is None:
             raise ValueError("Parameters not estimated. Call fit() first.")
         b, c, e = self.params
