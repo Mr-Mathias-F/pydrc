40226f63715e05d6c26c65b8133ff80c13a63500
Hallvard A. Wæhler <60000604+hallvaaw@users.noreply.github.com>
2023-10-06 12:27:21 +0200

    Update exponential_decay.py

--- Functions/exponential_decay.py
+++ Functions/exponential_decay.py
@@ -4,21 +4,20 @@ from scipy.optimize import curve_fit
 
 class ExponentialDecay:
     def __init__(self):
-        """Initialize an ExponentialDecayModel instance with parameters set to None"""
         self.params = None
 
     def exponential_decay(self, x, c, d, e):
-        """Calculate the exponential decay model at a given point"""
+        """Calculate exponential decay model"""
         f = c + (d - c) * np.exp(-x / e)
         return f
 
     def fit(self, x_data, y_data):
-        """Estimate the parameters (c, d, e) of the exponential decay model from data"""
+        """Estimate parameters (c, d, e)"""
         params, _ = curve_fit(self.exponential_decay, x_data, y_data)
         self.params = params
 
     def predict(self, x):
-        """Predict the output of the exponential decay model for a given input using estimated parameters"""
+        """Predict output of the exponential decay model"""
         if self.params is None:
             raise ValueError("Parameters not estimated. Call fit() first.")
         c, d, e = self.params
