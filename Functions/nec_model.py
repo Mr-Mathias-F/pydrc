cfc5bba92b04309305c3d81834e1f4e14c8926b5
Hallvard A. Wæhler <60000604+hallvaaw@users.noreply.github.com>
2023-10-07 11:09:17 +0200

    Update nec_model.py

--- Functions/nec_model.py
+++ Functions/nec_model.py
@@ -4,21 +4,20 @@ from scipy.optimize import curve_fit
 
 class NecModel:
     def __init__(self):
-        """Initialize a NecModel instance with parameters set to None"""
         self.params = None
 
     def nec_model(self, x, c, b, d, e, b_2, d_2, e_2, I):
-        """Calculate the no effect concentration model at a given point"""
+        """Calculate the no effect concentration model"""
         f = c + (d - c) * np.exp(-b * (x - e) * I * (x - e)) + ((d_2) / (1 + np.exp(b_2 * (np.log(x)) - np.log(e_2))))
         return f
 
     def fit(self, x_data, y_data):
-        """Estimate the parameters (c, b, d, e, b_2, d_2, e_2, I) of the no effect concentration model from data"""
+        """Estimate parameters (c, b, d, e, b_2, d_2, e_2, I)"""
         params, _ = curve_fit(self.nec_model, x_data, y_data)
         self.params = params
 
     def predict(self, x):
-        """Predict the output of the no effect concentration model for a given input using estimated parameters"""
+        """Predict the output of the no effect concentration model"""
         if self.params is None:
             raise ValueError("Parameters not estimated. Call fit() first.")
         c, b, d, e, b_2, d_2, e_2, I = self.params
