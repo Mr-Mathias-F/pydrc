739e24950cf8e8decc1bf2c934de2bb305655bef
Hallvard A. Wæhler <60000604+hallvaaw@users.noreply.github.com>
2023-10-08 15:58:51 +0200

    Update two_phase.py

--- Functions/two_phase.py
+++ Functions/two_phase.py
@@ -4,21 +4,20 @@ from scipy.optimize import curve_fit
 
 class TwoPhaseModel:
     def __init__(self):
-        """Initialize a TwoPhaseModel instance with parameters set to None"""
         self.params = None
 
     def two_phase(self, x, c, b_1, b_2, d_1, d_2, e_1, e_2):
-        """Calculate the two-phase function at a given point"""
+        """Calculate two-phase function"""
         f = c + ((d_1 - c) / (1 + np.exp(b_1 * (np.log(x) - np.log(e_1))))) + (d_2 / (1 + np.exp(b_2 * (np.log(x) - np.log(e_2)))))
         return f
 
     def fit(self, x_data, y_data):
-        """Estimate the parameters (c, b_1, b_2, d_1, d_2, e_1, e_2) of the two-phase function from data"""
+        """Estimate parameters (c, b_1, b_2, d_1, d_2, e_1, e_2)"""
         params, _ = curve_fit(self.two_phase, x_data, y_data)
         self.params = params
 
     def predict(self, x):
-        """Predict the output of the two-phase function for a given input using estimated parameters"""
+        """Predict the output of the two-phase function"""
         if self.params is None:
             raise ValueError("Parameters not estimated. Call fit() first.")
         c, b_1, b_2, d_1, d_2, e_1, e_2 = self.params
