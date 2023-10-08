67f556a5de62e71cedcfeaa4738fee7976c8c747
Hallvard A. Wæhler <60000604+hallvaaw@users.noreply.github.com>
2023-10-08 15:59:34 +0200

    Update weibull_P2.py

--- Functions/weibull_P2.py
+++ Functions/weibull_P2.py
@@ -3,21 +3,20 @@ import numpy as np
 
 class WeibullP2:
     def __init__(self):
-        """Initialize a WeibullP2Model instance with parameters set to None"""
         self.params = None
 
     def weibull_P2(self, x, b, e):
-        """Calculate the two-parameter Weibull function at a given point"""
+        """Calculate two-parameter Weibull function"""
         f = np.exp(-np.exp(b * (np.log(x) - e)))
         return f
 
     def fit(self, x_data, y_data):
-        """Estimate the parameters (b, e) of the two-parameter Weibull function from data"""
+        """Estimate parameters (b, e)"""
         params, _ = curve_fit(self.weibull_P2, x_data, y_data)
         self.params = params
 
     def predict(self, x):
-        """Predict the output of the two-parameter Weibull function for a given input using estimated parameters"""
+        """Predict the output of the two-parameter Weibull function"""
         if self.params is None:
             raise ValueError("Parameters not estimated. Call fit() first.")
         b, e = self.params
