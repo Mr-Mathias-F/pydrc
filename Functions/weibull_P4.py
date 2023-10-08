b6cf78aadf726460b164031a569e5dbcb705eb3a
Hallvard A. Wæhler <60000604+hallvaaw@users.noreply.github.com>
2023-10-08 16:01:35 +0200

    Update weibull_P4.py

--- Functions/weibull_P4.py
+++ Functions/weibull_P4.py
@@ -3,21 +3,20 @@ import numpy as np
 
 class WeibullP4:
     def __init__(self):
-        """Initialize a WeibullP4Model instance with parameters set to None"""
         self.params = None
 
     def weibull_P4(self, x, b, c, d, e):
-        """Calculate the four-parameter Weibull function at a given point"""
+        """Calculate four-parameter Weibull function"""
         f = c + (d - c) * np.exp(-np.exp(b * (np.log(x) - np.log(e))))
         return f
 
     def fit(self, x_data, y_data):
-        """Estimate the parameters (b, c, d, e) of the four-parameter Weibull function from data"""
+        """Estimate the parameters (b, c, d, e)"""
         params, _ = curve_fit(self.weibull_P4, x_data, y_data)
         self.params = params
 
     def predict(self, x):
-        """Predict the output of the four-parameter Weibull function for a given input using estimated parameters"""
+        """Predict the output of the four-parameter Weibull function"""
         if self.params is None:
             raise ValueError("Parameters not estimated. Call fit() first.")
         b, c, d, e = self.params
