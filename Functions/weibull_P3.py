4ac36dd03b72ca95984776633a527dd60e0ea00a
Hallvard A. Wæhler <60000604+hallvaaw@users.noreply.github.com>
2023-10-08 16:00:49 +0200

    Update weibull_P3.py

--- Functions/weibull_P3.py
+++ Functions/weibull_P3.py
@@ -3,21 +3,20 @@ import numpy as np
 
 class WeibullP3Model:
     def __init__(self):
-        """Initialize a WeibullP3Model instance with parameters set to None"""
         self.params = None
 
     def weibull_P3(self, x, b, d, e):
-        """Calculate the three-parameter Weibull function at a given point"""
+        """Calculate three-parameter Weibull function"""
         f = (d - 0) * np.exp(-np.exp(b * (np.log(x) - e)))
         return f
 
     def fit(self, x_data, y_data):
-        """Estimate the parameters (b, d, e) of the three-parameter Weibull function from data"""
+        """Estimate parameters (b, d, e)"""
         params, _ = curve_fit(self.weibull_P3, x_data, y_data)
         self.params = params
 
     def predict(self, x):
-        """Predict the output of the three-parameter Weibull function for a given input using estimated parameters"""
+        """Predict the output of the three-parameter Weibull function"""
         if self.params is None:
             raise ValueError("Parameters not estimated. Call fit() first.")
         b, d, e = self.params
