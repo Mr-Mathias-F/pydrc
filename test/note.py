c9b478c97f051c43f06cf5810dca9d63f03e1b40
Mathias Fon <74455376+Mr-Mathias-F@users.noreply.github.com>
2024-03-25 13:34:35 +0100

    Create note.py
    Early plans for implementation of curve ID (grouping variable)

---
+++ test/note.py
@@ -0,0 +1,14 @@
+pydrc.model(df, x, y, function, group)
+
+if group != None:
+  for i in group:
+    self.data[group]
+    curve_fit()
+    STORE PARAMETERS HERE (Sum error squared)
+
+# Application: compound 'A' vs compound 'B'
+  # model.fit() yields:
+  # summary()
+  # predict() based on grouping
+  # plot() based on grouping
+  # residual error (per individual model and across model by summation of squared error and dividing by residual DF)
