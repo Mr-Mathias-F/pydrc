5b4fae1c8ba142c67480a2bb83b2614cf163e3df
Hallvard A. Wæhler <60000604+hallvaaw@users.noreply.github.com>
2024-06-19 19:36:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,15 @@
-"""
-pydrc: Python module for fitting, analysis and visualization of dose-response data
-"""
-
 from setuptools import setup
-
 
 setup(
     name='pydrc',
-    version='0.0.1',
+    version='0.1.0',
     description='Python module for fitting, analysis and visualization of dose-response data',
     license='MIT'
     authors='Mathias Fon, Hallvard Wæhler',
     url='https://github.com/Mr-Mathias-F/pydrc',
     packages=['pydrc'],
-    install_requires=read_requirements(),
+    install_requires=['numpy', 'scipy', 'pandas'],
     tests_require='pytest',
     python_requires='>=3.8',
     zip_safe=True,
 )
-
