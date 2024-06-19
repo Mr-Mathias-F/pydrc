631ef005b5f1871c2dbbc53b4f5932d75cd3a7f3
hallvaaw <hallvard_wahler@hotmail.com>
2024-06-19 19:26:40 +0200

    add __init__.py

---
+++ pydrc/__init__.py
@@ -0,0 +1,15 @@
+from .ar_model import ARModel
+from .exponential_decay import ExponentialDecayModel
+from .gompertz_model import GompertzModel
+from .hill_equation import HillEquationModel
+from .logistic_P2 import LogisticP2Model
+from .logistic_P3 import LogisticP3Model
+from .logistic_P4 import LogisticP4Model
+from .michaelis_menten import MichaelisMentenModel
+from .nec_model import NecModel
+from .two_phase import TwoPhaseModel
+from .weibull_P2 import WeibullP2Model
+from .weibull_P3 import WeibullP3Model
+from .weibull_P4 import WeibullP4Model
+
+__all__ = ['ARModel', 'ExponentialDecayModel', 'GompertzModel', 'HillEquationModel', 'LogisticP2Model', 'LogisticP3Model', 'LogisticP4Model', 'MichaelisMentenModel', 'NecModel', 'TwoPhaseModel', 'WeibullP2Model', 'WeibullP3Model', 'WeibullP4Model']
